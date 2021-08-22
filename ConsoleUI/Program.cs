cdd3deb37e1fafefbd0ddbac160edf16928da1b0
Selahattin Yunus Yavuz <yunusssyavuzzz@hotmail.com>
2021-08-23 01:48:58 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using System;
+using DataAccess.Abstract;
+using Business.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager( new InMemoryProductDal());
+
+            foreach (var p in productManager.GetAll())
+            {
+                Console.WriteLine(p.ProductName);
+            }
+           
+
+        }
+    }
+}
